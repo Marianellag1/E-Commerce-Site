00a43dfc6338a65887867cdde79beec92ac74cc4
Mgamboa <marianellag400@gmail.com>
2022-10-05 18:35:59 -0500

    connected config folder to server.js, .env file was updated with username and password, schema was also ran. and source was taken to show in bash.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,3 +1,5 @@
+//connected to server.js
+//.env has db name, username, password
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
@@ -7,9 +9,11 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      //port: 3306 (?)
       dialectOptions: {
         decimalNumbers: true,
       },
     });
 
 module.exports = sequelize;
+
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
+//Codes were given 
 const express = require('express');
-const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
+//importing routes
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync().then(() =>{
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  
 });
